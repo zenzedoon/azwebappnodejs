f68f0d57b48247ccaef598faaf1896539f669951
Zied BEN SALEM <50891244+zenzedoon@users.noreply.github.com>
2023-02-15 11:20:50 +0100

    update example (#2)

---
+++ Example/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.14.9"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
