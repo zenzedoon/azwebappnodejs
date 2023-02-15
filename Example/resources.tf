0fcd719c72e9797699a4706be39c0fd42ec5f709
Zied BEN SALEM <ziedbensalem1986@gmail.com>
2023-02-15 11:20:05 +0100

    update example

--- Example/resources.tf
+++ Example/resources.tf
@@ -2,18 +2,29 @@ resource "azurerm_resource_group" "rgwebapp" {
   name     = "rg-webapp"
   location = "West Europe"
 }
-resource "azurerm_source_control_token" "githubtoken" {
-  type  = "GitHub"
-  token = "********************************" # To be Defined 
- }
 
-module "azwebapptest01" {
+data "azurerm_resource_group" "rg-dns"{
+  name    = "rg-dns"
+
+}
+
+data "azurerm_dns_zone" "customdomain"{
+  name = "customdomain.tk"  
+   resource_group_name = data.azurerm_resource_group.rg-dns.name
+}
+
+module "azwebapptest02" {
   source  = "github.com/zenzedoon/azwebappnodejs"
   spname  = "spwebapp001"
   splocation = "West Europe"
   rgname  = azurerm_resource_group.rgwebapp.name
-  azwebappname  = "azwebapptest01"
-  githubtoken = azurerm_source_control_token.githubtoken.token
+  azwebappname  = "azwebapptest02"
+  azwebappkeys = {
+    "Letsencyptenabled" = true
+  }
   githubrepo  = "https://github.com/zenzedoon/nodejs-docs-hello-world"
-  
+  githubtokenvalue ="TokenmustbegeneratedfrtomGitHub"
+  app_domainevalue = data.azurerm_dns_zone.customdomain.name
+  app_cnamevalue = "www"
+  rg_dnszone = data.azurerm_resource_group.rg-dns.name
 }
